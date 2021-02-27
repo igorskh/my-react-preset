3c63627c8e967133a5ff465c2d7725b32c5dce4c
Igor Kim <igor.skh@gmail.com>
2021-02-27 17:28:59 +0100

    add sample component

---
+++ src/components/Sample.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Child from "./Child";
+
+// import "./Sample.css";
+
+type AppProps = {
+    title: string,
+    number: number
+}
+
+const Sample = ({title, number}: AppProps): JSX.Element => {
+    const [counter, setCounter] = useState<number>(number);
+
+    function changeCounter(diff: number) {
+        setCounter(p => p + diff);
+    }
+
+    return (
+        <div id="block">
+            <h1>{title}</h1>
+            <h2>Count is {counter}</h2>
+
+            <Child text={"test"} />
+
+            <button onClick={() => changeCounter(1)}>+1</button>
+            <button onClick={() => changeCounter(-1)}>-1</button>
+        </div>
+    );
+};
+
+export default Sample;
