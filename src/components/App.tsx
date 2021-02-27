9d09b369cf7f3a280d587c10fa62bfdcd01d9ed1
Igor Kim <igor.skh@gmail.com>
2021-02-27 15:58:23 +0100

    add useState example

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 
-const App = (): JSX.Element => {
+type AppProps = {
+    title: string,
+    number: number
+}
+
+const App = ({title, number}: AppProps): JSX.Element => {
+    const [counter, setCounter] = useState<number>(number);
+
+    function changeCounter(diff: number) {
+        setCounter(p => p + diff);
+    }
+
     return (
         <div id="block">
-            <h1>Component1 content</h1>
+            <h1>{title}</h1>
+            <h2>Count is {counter}</h2>
+
+            <button onClick={() => changeCounter(1)}>+1</button>
+            <button onClick={() => changeCounter(-1)}>-1</button>
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
+ReactDOM.render(
+    <App title={"Hello Typescript World"} number={1} />,
+    document.getElementById("root")
+);
