22593494637b2ed8bc0cacdc4f6f99e98cca4c41
Igor Kim <igor.skh@gmail.com>
2021-02-28 11:21:47 +0100

    replace enzyme with react-testing-library

--- jest.config.js
+++ jest.config.js
@@ -11,9 +11,6 @@ module.exports = {
   // An array of file extensions your modules use
   moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx"],
 
-  // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ["<rootDir>/enzyme.config.js"],
-
   // The test environment that will be used for testing
   testEnvironment: "jsdom",
 
